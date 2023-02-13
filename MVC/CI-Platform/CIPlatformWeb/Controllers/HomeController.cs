67855b4248e70a41eeba911af27a4311312ac4d2
JansariShubham <shubhamjansari11@gmail.com>
2023-02-13 18:34:04 +0530

    Working on bulkybook project

--- MVC/CI-Platform/CIPlatformWeb/Controllers/HomeController.cs
+++ MVC/CI-Platform/CIPlatformWeb/Controllers/HomeController.cs
@@ -22,6 +22,20 @@ namespace CIPlatformWeb.Controllers
         {
             return View();
         }
+        public IActionResult ForgotPassword()
+        {
+            return View();
+        }
+
+        public IActionResult ResetPassword()
+        {
+            return View();  
+        }
+
+        public IActionResult Registration()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
